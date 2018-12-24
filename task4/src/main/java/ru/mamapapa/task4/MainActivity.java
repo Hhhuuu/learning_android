0e9ba4e35e99bbe0be95e382accad973beb7d201
m_amapapa <popovmax2602@gmail.com>
2018-12-24 21:46:50 +0300

    Task4 Обновление импорта

--- task4/src/main/java/ru/mamapapa/task4/MainActivity.java
+++ task4/src/main/java/ru/mamapapa/task4/MainActivity.java
@@ -2,17 +2,12 @@ package ru.mamapapa.task4;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import static android.content.Intent.FLAG_INCLUDE_STOPPED_PACKAGES;
 import static ru.mamapapa.task4.CustomBroadcastReceiver.ACTION;
-import static ru.mamapapa.task4.CustomBroadcastReceiver.BROADCAST_DATA_KEY;
 
 public class MainActivity extends AppCompatActivity {
     private CustomBroadcastReceiver broadcastReceiver;
